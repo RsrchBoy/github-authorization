ac74819d6caa3a5d48867ca2c76fa927c6423aa6
Chris Weyl <cweyl@alumni.drew.edu>
2012-12-23 13:54:52 -0600

    initial commit

---
+++ lib/GitHub/Authorization.pm
@@ -0,0 +1,19 @@
+package GitHub::Authorization;
+
+# ABSTRACT: The great new GitHub::Authorization!
+
+use Moose;
+use namespace::autoclean;
+
+
+__PACKAGE__->meta->make_immutable;
+!!42;
+__END__
+
+=head1 SYNOPSIS
+
+=head1 DESCRIPTION
+
+=head1 SEE ALSO
+
+=cut
